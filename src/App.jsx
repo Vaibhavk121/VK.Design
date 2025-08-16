76c03c0beb2ed3745ba606be127105ea8c648346
vaibhavk121 <vaibhavkumar10112004@gmail.com>
2025-08-16 13:30:17 +0530

    Filename mismatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './style.css'
 
-import Navbar from "./components/Navbar";
-import Hero from "./sections/Hero";
-import Features from './sections/Features';
-import About from "./sections/About";
-import Contact from "./sections/Contact";
-import Footer from "./sections/Footer";
+import Navbar from "./components/navbar";
+import Hero from "./sections/hero";
+import Features from './sections/features';
+import About from "./sections/about";
+import Contact from "./sections/contact";
+import Footer from "./sections/footer";
 
 function App() {
   const [count, setCount] = useState(0)
